22a41acb908a746378d1735d8046638a410b64e7
JohnsonNYC <jkow95@gmail.com>
2021-02-04 15:00:47 -0500

    Included Multer and Sharp to upload files and resize my images

--- src/models/user.js
+++ src/models/user.js
@@ -48,7 +48,10 @@ const userSchema = new mongoose.Schema({
       type: String,
       required: true
     }
-  }]
+  }],
+  avatar:{
+    type: Buffer
+  }
 }, {
   timestamps: true
 })
@@ -66,6 +69,7 @@ userSchema.methods.toJSON = function(){
   const userObj = user.toObject()
   delete userObj.password 
   delete userObj.tokens
+  delete userObj.avatar
 
   return userObj
 }
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,10 +2,13 @@ const express = require('express')
 const router = new express.Router(); // attach router to project with app.use() inside of index.js
 const User = require('../models/user')// Accesses model to be used at differenct endpoints
 const auth = require('../middleware/auth')
-const multer = require('multer')
+const multer = require('multer') // Allows file upload capabilities 
+const sharp = require('sharp') // convert large images into usable format 
+
 // Public Routes include sign up and login 
 // Everything else will require an authentication token
 
+// Create  User
 router.post('/users', async (req,res)=>{
   // Needs access to User model to create new User (line 3)
   // req.body hold all incoming information 
@@ -20,6 +23,7 @@ router.post('/users', async (req,res)=>{
   }
 })
 
+//Logout User
 router.post('/users/login', async (req,res) => {
   // find user by email and then use bcrypt compare method to compare passwords
   // Create reusable function that does all that for us
@@ -34,13 +38,51 @@ router.post('/users/login', async (req,res) => {
   }
 })
 
+// Add Image
 const upload = multer({
-  dest:'avatars'
-})
-router.post('/users/me/avatar', upload.single('avatar'),(req,res) => {
-  res.send()
+  limits:{
+    fileSize: 1000000
+  },
+  fileFilter(req,file,cb){
+    if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+      return cb(new Error('Pleaser upload an image'))
+    }
+    cb(undefined, true)
+  }
 })
 
+router.post('/users/me/avatar', auth, upload.single('avatar'),async (req,res) => {
+  const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+  req.user.avatar = buffer 
+  await req.user.save()
+  res.send()
+}, (error,req,res,next) => {
+  res.status(400).send({error: error.message})
+})
+
+// Delete Image
+router.delete('/users/me/avatar', auth, async (req,res) => {
+  req.user.avatar = undefined
+  await req.user.save()
+  res.send() 
+})
+
+//Read Image 
+router.get('/users/:id/avatar', async (req,res) => {
+  try{
+    const user = await User.findById(req.params.id);
+
+    if(!user || !user.avatar){
+      throw new Error()
+    }
+    // setup response header 
+    res.set('Content-Type','image/jpg') // Default to Content-Type: 'application/json`'
+    res.send(user.avatar)
+  }catch(e){
+    res.status(404).send()
+  }
+})
+// Logout User
 router.post('/users/logout', auth, async (req,res) => {
   try{
     req.user.tokens = req.user.tokens.filter((token) => {
@@ -63,6 +105,7 @@ router.post('/users/logoutAll', auth, async (req,res) => { // remove all tokens 
   }
 })
 
+// Read User
 router.get('/users/me',auth ,async (req,res) => {
   res.send(req.user)
 })
