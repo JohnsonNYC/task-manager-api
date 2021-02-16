44224c8815d49ce28a8df97167acfb965b3f1e82
JohnsonNYC <jkow95@gmail.com>
2021-02-16 12:25:10 -0500

    Added tests for Tasks

--- test/fixtures/db.js
+++ test/fixtures/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const User = require('../../src/models/user')
-const userOneID = new mongoose.Types.ObjectId(); // Needed for ID and Tokens 
+const Task = require('../../src/models/task')
 
+const userOneID = new mongoose.Types.ObjectId(); // Needed for ID and Tokens 
 const userOne = ({
   _id: userOneID,
   name:'Mike',
@@ -13,14 +14,57 @@ const userOne = ({
   }]
 })
 
+const userTwoID = new mongoose.Types.ObjectId(); // Needed for ID and Tokens 
+const userTwo = ({
+  _id: userTwoID,
+  name:'Andrew',
+  email: 'andrews@example.com',
+  password: 'Myhouse099!',
+  tokens:[{
+    token: jwt.sign({_id: userTwoID}, process.env.JWT_SECRET)
+  }]
+})
+
+const taskOne = {
+  _id: new mongoose.Types.ObjectId(),
+  description: 'First task',
+  completed: false,
+  owner: userOne._id
+}
+
+const taskTwo = {
+  _id: new mongoose.Types.ObjectId(),
+  description: 'Second task',
+  completed: false,
+  owner: userOne._id
+}
+
+const taskThree = {
+  _id: new mongoose.Types.ObjectId(),
+  description: 'Third task',
+  completed: false,
+  owner: userTwo._id
+}
+
 const setUpDatabase = async () => { // we need the example user in user.test.js in beforeEach function
   // wipe all users in database 
   await User.deleteMany()
+  await Task.deleteMany()
+
   await new User(userOne).save()
+  await new User(userTwo).save()
+  await new Task(taskOne).save()
+  await new Task(taskTwo).save()
+  await new Task(taskThree).save()
 }
 
 module.exports = {
   userOneID,
   userOne,
+  userTwoID,
+  userTwo,
+  taskOne,
+  taskTwo,
+  taskThree,
   setUpDatabase
 }
--- test/task.test.js
+++ test/task.test.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const app = require('../src/app')
 
 const Task = require('../src/models/task')
-const {userOneID, userOne, setUpDatabase} = require('./fixtures/db')
+const {userOneID, userOne, userTwo, userTwoID, setUpDatabase, taskOne, taskTwo, taskThree} = require('./fixtures/db')
 
 beforeEach(setUpDatabase)// provided by Jest and will be used to wipe out database. Lifecycle method
 
@@ -20,3 +20,49 @@ test('Should create task for user', async() => {
     expect(task).not.toBe()
     expect(task.completed).toBe(false)
 })
+
+test('Should get all tasks from user one', async() => {
+  const response = await request(app)
+  .get('/tasks')
+  .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+  .expect(200)
+
+  expect(response.body.length).toBe(2)
+})
+
+test('Should not delete other users tasks', async()=>{
+  const response = await request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set("Authorization", `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404)
+
+  // Asser that task is still in the database 
+  const task = Task.findById(`${taskOne._id}`)
+  expect(task).not.toBe()
+})
+
+//Extra tests to create 
+//
+// User Test Ideas
+//
+// Should not signup user with invalid name/email/password
+// Should not update user if unauthenticated
+// Should not update user with invalid name/email/password
+// Should not delete user if unauthenticated
+
+//
+// Task Test Ideas
+//
+// Should not create task with invalid description/completed
+// Should not update task with invalid description/completed
+// Should delete user task
+// Should not delete task if unauthenticated
+// Should not update other users task
+// Should fetch user task by id
+// Should not fetch user task by id if unauthenticated
+// Should not fetch other users task by id
+// Should fetch only completed tasks
+// Should fetch only incomplete tasks
+// Should sort tasks by description/completed/createdAt/updatedAt
+// Should fetch page of tasks
